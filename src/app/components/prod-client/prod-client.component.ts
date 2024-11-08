e576f4a7c86d85e494405040bbaad1c27ecd1007
juanmontrasi <juanmontrasi@gmail.com>
2024-11-08 17:38:47 -0300

    Busqueda con filtro en el backend dentro de la pesaña productos

--- src/app/components/prod-client/prod-client.component.ts
+++ src/app/components/prod-client/prod-client.component.ts
@@ -3,34 +3,51 @@ import { Product } from '../../interfaces/product.js';
 import { Router } from '@angular/router';
 import { ProductsService } from '../../services/products.service.js';
 import { ToastrService } from 'ngx-toastr';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { FilterPipeModule } from 'ngx-filter-pipe';
-import { OrdersService } from '../../services/orders.service.js';
-import { CartService } from '../../services/cart.service.js';
+import { ProductService } from '../../services/product.service.js';
 
 
 @Component({
   selector: 'app-prod-client',
   standalone: true,
-  imports: [FormsModule, CommonModule, FilterPipeModule],
+  imports: [FormsModule, CommonModule, FilterPipeModule, ReactiveFormsModule],
   templateUrl: './prod-client.component.html',
   styleUrl: './prod-client.component.scss'
 })
 export class ProdClientComponent {
-
+  productName: string = '';
   listProd: any[] = [];
-  searchInput = { nombre_producto: '', desc_producto: '', stock: '', precio: '' };
+  formSearch: FormGroup;
 
-  constructor(private router: Router, private _productService: ProductsService, private toastr: ToastrService) { }
+  constructor(
+    private router: Router,
+    private toastr: ToastrService,
+    private _productService: ProductService,
+    private fb: FormBuilder) {
+    this.formSearch = this.fb.group({
+      productName: ['', Validators.required]
+    })
+  }
+
+
 
   ngOnInit(): void {
     this.getProducts();
+
   }
 
   getProducts() {
     this._productService.getProducts().subscribe(products => {
       this.listProd = products;
+    });
+  }
+
+  getProdByName() {
+    this._productService.getProductsByName(this.formSearch.value.productName).subscribe(products => {
+      this.listProd = products;
+      console.log('Productos encontrados:', this.listProd);
     });
   }
 
